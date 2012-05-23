b5c4742a9102f6bd86e2789ce52369cc78ac10c8
Bryan Dunsmore <dunsmoreb@gmail.com>
2012-05-23 16:21:21 -0500

    Add outline for client network features
    Add structures and prototypes of functions to be used by a client to
    communicate with a server.

---
+++ src/network/client.h
@@ -0,0 +1,20 @@
+#ifndef NETWORK_CLIENT_H
+#define NETWORK_CLIENT_H
+
+#include "network.h"
+
+int il_Network_connect(const struct {unsigned length; char* data} address, il_Network_Connection* ptr);
+
+int il_Netowrk_authenticate(il_Network_Connection* ptr, il_Network_Authcode auth);
+
+typedef struct {
+  unsigned short version;
+  unsigned players;
+  float latency;
+} il_Network_ServerInfo;
+
+int il_Network_probe(il_Network_Connection* ptr, il_Network_ServerInfo* info);
+
+int il_Network_disconnect(il_Network_Connection* ptr);
+
+#endif
