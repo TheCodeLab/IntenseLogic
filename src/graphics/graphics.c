4deeec85041244cab4cc04c91ace780972203675
Harutsedo <Colby.L.Wolfe@gmail.com>
2012-07-28 03:01:15 -0400

    On load, a window is created and can be closed.

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -1,5 +1,13 @@
 #include "graphics.h"
+#include "sdl/SDL.h"
+
+SDL_Surface* canvas;
 
 void il_Graphics_init() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	canvas = SDL_SetVideoMode(800, 600, 32, SDL_DOUBLEBUF);
+}
 
+void il_Graphics_quit() {
+	SDL_Quit();
 }
