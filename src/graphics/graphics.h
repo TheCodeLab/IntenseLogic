82c45a426e0b7b1a5cedc3dd6ec7986c42cfd9f2
tiffany bennett <tiffany@physibots.info>
2012-07-29 00:38:03 -0400

    Change main() to send both tick and frame events from the same loop.
    This evades having to get into a mess of threading (at least for now).

--- src/graphics/graphics.h
+++ src/graphics/graphics.h
@@ -4,6 +4,8 @@
 #define IL_GRAPHICS_RANGE   (2)
 #define IL_GRAPHICS_TICK    ((IL_GRAPHICS_RANGE<<8) + 0)
 
+#define IL_GRAPHICS_TICK_LENGTH (16666)
+
 void il_Graphics_init();
 void il_Graphics_draw();
 void il_Graphics_quit();
