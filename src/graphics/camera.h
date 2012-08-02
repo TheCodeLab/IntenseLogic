315ac16041290a693c7169878e6a085ec2533839
Harutsedo <Colby.L.Wolfe@gmail.com>
2012-08-02 12:51:24 -0400

    Added camera, which can be controlled using up and down

---
+++ src/graphics/camera.h
@@ -0,0 +1,11 @@
+#include "common/positionable.h"
+
+typedef struct il_Graphics_Camera {
+        il_Common_Positionable* positionable;
+} il_Graphics_Camera;
+
+il_Graphics_Camera* il_Graphics_Camera_new();
+
+void il_Graphics_Camera_render(il_Graphics_Camera* camera);
+
+void il_Graphics_Camera_translate(il_Graphics_Camera* camera, float x, float y, float z);
