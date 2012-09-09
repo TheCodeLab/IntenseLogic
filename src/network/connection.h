91e9de880e1d9b8ef0a3221f817033d779443b69
tiffany <tiffany@physibots.info>
2012-09-09 15:23:53 -0400

    Connection handling code using libevent

--- src/network/connection.h
+++ src/network/connection.h
@@ -2,13 +2,27 @@
 #define IL_NETWORK_CONNECTION_H
 
 #include <stdlib.h>
+#include <event2/util.h>
+
+struct evbuffer;
+
+#include "common/string.h"
 
 typedef struct il_Network_Connection il_Network_Connection;
 
 il_Network_Connection * il_Network_Connection_new();
 
+int il_Network_Connection_connect(il_Network_Connection *, const char * host, unsigned short port);
+int il_Network_Connection_setSocket(il_Network_Connection * con, evutil_socket_t fd, int flags);
+int il_Network_Connection_bind(il_Network_Connection *, const char * host, unsigned short port);
+int il_Network_Connection_disconnect(il_Network_Connection *, il_Common_String reason);
+
 const char * il_Network_Connection_getError(il_Network_Connection *);
 
-int il_Network_Connection_write(il_Network_Connection *, void * data, size_t n);
+int il_Network_Connection_write(il_Network_Connection * con, ev_uint32_t tag, const void * data, size_t n);
+int il_Network_Connection_write_buffer(il_Network_Connection * con, ev_uint32_t tag, struct evbuffer * in);
+
+int il_Network_Connection_read(il_Network_Connection * con, ev_uint32_t * ptag, void ** data);
+int il_Network_Connection_read_buffer(il_Network_Connection * con, ev_uint32_t *ptag, struct evbuffer * data);
 
 #endif
