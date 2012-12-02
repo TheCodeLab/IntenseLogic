04abb8f2630359866ab0e5d07de1fba5c2cdd56f
Cameron Moy <cameronmmoy@gmail.com>
2012-12-02 09:58:19 -0500

    Fix event system

--- src/common/common.h
+++ src/common/common.h
@@ -4,4 +4,10 @@
 #include "common/log.h"
 #include "common/entity.h"
 
+#include "graphics/graphics.h"
+#include "network/network.h"
+/* #include "physics/physics.h" */
+#include "script/script.h"
+#include "asset/asset.h"
+
 void il_init();
