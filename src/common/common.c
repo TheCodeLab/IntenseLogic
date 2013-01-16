69cf42efce8c605dc7c7f11f88065f29eaedc30e
tiffany <tiffany@stormbit.net>
2013-01-16 05:40:30 -0500

    Redid events and fixed all warnings
    The event system no longer relies on global variables, and the API
    should be complete enough for a migration to threading without
    destructive changes.
    
    There is only one event queue right now, and I will
    sort out what to do with that later. I would like to have one event
    queue per module so that ID conflicts will never arise, without the
    stupid scheme that we had before.
    
    The callback registration mechanism is more robust now, allowing you to
    insert your callbacks before, after, replace, or just ignore the others,
    contrast this with the previous first-come-first-serve mechanism.

--- src/common/common.c
+++ src/common/common.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+void ilE_init();
+void ilI_init();
+
 void il_init()
 {
     ilE_init();
--- src/common/input.h
+++ src/common/input.h
@@ -1,13 +1,12 @@
 #ifndef COMMON_INPUT_H
 #define COMMON_INPUT_H
 
-#define IL_INPUT_RANGE      (1)
-#define IL_INPUT_KEYDOWN    ((IL_INPUT_RANGE << 8) + 0) // int
-#define IL_INPUT_KEYUP      ((IL_INPUT_RANGE << 8) + 1) // int
-#define IL_INPUT_MOUSEDOWN  ((IL_INPUT_RANGE << 8) + 2) // int
-#define IL_INPUT_MOUSEUP    ((IL_INPUT_RANGE << 8) + 3) // int
-#define IL_INPUT_MOUSEMOVE  ((IL_INPUT_RANGE << 8) + 4) // ilI_mouseMove
-#define IL_INPUT_MOUSEWHEEL ((IL_INPUT_RANGE << 8) + 5) // ilI_mouseWheel
+#define IL_INPUT_KEYDOWN    (3) // int
+#define IL_INPUT_KEYUP      (4) // int
+#define IL_INPUT_MOUSEDOWN  (5) // int
+#define IL_INPUT_MOUSEUP    (6) // int
+#define IL_INPUT_MOUSEMOVE  (7) // ilI_mouseMove
+#define IL_INPUT_MOUSEWHEEL (8) // ilI_mouseWheel
 // note: the UP/DOWN functions use the glfw values.
 
 typedef struct ilI_mouseMove {
