1acd3311d7d4cf0dc9aaaabf09b7a6ef6df9643b
tiffany <tiffany@stormbit.net>
2013-01-28 16:34:39 -0500

    OBJ loader converts quads to triangles
    Got rid of graphics.lua because it is bad practice, as well

--- script/graphics.lua
+++
@@ -1,6 +0,0 @@
-_G.drawable = require "drawable";
-_G.context  = require "context";
-_G.texture  = require "texture";
-_G.material = require "material";
-_G.camera   = require "camera";
-
--- test.lua
+++ test.lua
@@ -1,6 +1,10 @@
 package.path = package.path..";script/?.lua;script/graphics/?.lua;script/common/?.lua"
 local script = require "script"
-require "graphics"
+local drawable = require "drawable";
+local context  = require "context";
+local texture  = require "texture";
+local material = require "material";
+local camera   = require "camera";
 local positionable = require "positionable"
 local world = require "world"
 local matrix = require "matrix"
@@ -25,19 +29,32 @@ local c = context();
 c.world = w;
 w.context = c;
 c:setActive();
-local m = mesh("teapot.obj")
+local m;
 local t = texture.fromfile "white-marble-texture.png";
 local vf, ff = io.open("shaders/test.vert", "r"), io.open("shaders/test.frag", "r");
 local mtl = material(vf:read "*a", ff:read "*a", "test material", "in_Position", "in_Texcoord", nil, "mvp", {"tex"}, {1});
 print(mtl);
-for i = 0, 26 do
-    local box = positionable();
-    w:add(box);
-    box.drawable = m; --drawable.box;
-    box.material = mtl;
-    box.texture = t
-    box.position = vector3(i % 3, math.floor((i%9) / 3), math.floor(i/9)) * 10
-    box:track(c);
+if false then
+    m = mesh "minecraft.obj"
+    print(m.ptr)
+    local minecraft = positionable()
+    w:add(minecraft)
+    minecraft.drawable = m;
+    minecraft.material = mtl;
+    minecraft.texture = texture.default;
+    minecraft:track(c);
+else
+    m = mesh "teapot.obj"
+    local width = 1
+    for i = 0, width*width*width-1 do
+        local box = positionable();
+        w:add(box);
+        box.drawable = m; --drawable.box;
+        box.material = mtl;
+        box.texture = t
+        box.position = vector3(i % width, math.floor((i%(width*width)) / width), math.floor(i/(width*width))) * 15
+        box:track(c);
+    end
 end
 c.camera = camera(positionable(w));
 c.camera.projection_matrix = matrix.perspective(75, 4/3, 0.25, 1000);
