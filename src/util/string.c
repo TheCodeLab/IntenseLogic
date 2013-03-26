68b03eef454a5129339f72e29fb9f596acafe122
tiffany <tiffany@stormbit.net>
2013-03-25 21:42:04 -0400

    String management functions

--- src/util/string.c
+++ src/util/string.c
@@ -31,6 +31,9 @@ il_string *il_string_new(const char *s)
 
 il_string *il_string_copy(const il_string *old)
 {
+    if (!il_string_verify(old)) {
+        return NULL;
+    }
     il_string *str = calloc(1, sizeof(il_string));
     str->length = old->length;
     str->capacity = old->capacity;
@@ -43,9 +46,50 @@ il_string *il_string_copy(const il_string *old)
     return str;
 }
 
-const char *il_string_cstring(const il_string *s, size_t *len);
-il_string *il_string_ref(void* s);
-void il_string_unref(void* s);
+char *il_string_cstring(const il_string *s, size_t *len)
+{
+    if (!il_string_verify(s)) {
+        return NULL;
+    }
+    size_t size = strnlen(s->data, s->length);
+    if (len) {
+        *len = size;
+    }
+    int has_zero = size < s->length && s->data[size] == 0;
+    char *buf = calloc(1, strnlen(s->data, s->length) + !has_zero);
+    memcpy(buf, s->data, size);
+    if (!has_zero) {
+        buf[size] = 0;
+    }
+    return buf;
+}
+
+int il_string_verify(const il_string *s)
+{
+    return  s->canary == compute_canary(s) &&
+            s->data >= s->start &&
+            s->capacity >= s->length &&
+            (!s->refs || *s->refs > 0);
+}
+
+il_string *il_string_ref(void* ptr)
+{
+    il_string *s = ptr;
+    if (il_string_verify(s) && s->refs) {
+        (*s->refs)++;
+        return s;
+    }
+    return NULL;
+}
+
+void il_string_unref(void* ptr)
+{
+    il_string *s = ptr;
+    if (il_string_verify(s) && s->refs) {
+        (*s->refs)--;
+    }
+}
+
 il_string *il_string_sub(const il_string *s, int p1, int p2);
 int il_string_cmp(const il_string *a, const il_string *b);
 char il_string_byte(const il_string *s, int pos);
