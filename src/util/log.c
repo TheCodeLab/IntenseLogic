c48fa9b659241e146df55bc6e39058069b31b2e7
tiffany <tiffany@stormbit.net>
2013-03-27 01:34:55 -0400

    Initial logging utility functions

---
+++ src/util/log.c
@@ -0,0 +1,46 @@
+#include "log.h"
+
+#include <mowgli.h>
+
+const char *il_log_prefixes[] = {
+    "fatal: ",
+    "error: ",
+    "warning: ",
+    "",
+    "debug: "
+};
+
+
+mowgli_patricia_t *dict = NULL;
+
+int il_can_log(const char *file, int level)
+{
+    if (!dict) {
+        return level <= 2;
+    }
+    char *s = strdup(file);
+    if (strchr(s, '/')) {
+        *strchr(s, '/') = 0;
+    }
+    int *mlevel = mowgli_patricia_retrieve(dict, s);
+    free(s);
+    if (!mlevel) {
+        return level <= 2;
+    } else {
+        return level <= *mlevel;
+    }
+}
+
+void il_log_toggle(const char *module, int level)
+{
+    if (!dict) {
+        dict = mowgli_patricia_create(NULL);
+    }
+    int *data;
+    if ((data = mowgli_patricia_delete(dict, module)) == NULL) {
+        data = calloc(1, sizeof(int));
+    }
+    *data = level;
+    mowgli_patricia_add(dict, module, data);
+}
+
---
+++ src/util/log.h
@@ -0,0 +1,29 @@
+#ifndef ILU_LOG_H
+#define ILU_LOG_H
+
+#include <string.h>
+
+const char *il_log_prefixes[5];
+
+#define il_prettifyFile(name) \
+  (strstr(name, "src/")?strstr(name, "src/"):name)
+
+#define il_log_real(file, line, func, lvl, fmt, ...) \
+    mowgli_log_prefix_real(file, line, func, prefixes[lvl], fmt, __VA_ARGS__)
+
+#define il_debug(fmt, ...)      il_log_real(il_prettifyFile(__FILE__), __LINE__, __func__, 4, fmt, __VA_ARGS__)
+#define il_log(fmt, ...)        il_log_real(il_prettifyFile(__FILE__), __LINE__, __func__, 3, fmt, __VA_ARGS__)
+#define il_warning(fmt, ...)    il_log_real(il_prettifyFile(__FILE__), __LINE__, __func__, 2, fmt, __VA_ARGS__)
+#define il_error(fmt, ...)      il_log_real(il_prettifyFile(__FILE__), __LINE__, __func__, 1, fmt, __VA_ARGS__)
+#define il_fatal(fmt, ...) \
+    do { \
+        il_log_real(il_prettifyFile(__FILE__), __LINE__, __func__, 0, fmt, __VA_ARGS__); \
+        abort(); \
+    } while(0)
+
+int il_can_log(const char *file, int level);
+
+void il_log_toggle(const char *module, int level);
+
+#endif
+
