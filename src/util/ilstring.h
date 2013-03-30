d1a87da17baad74438c6f675f38d68f8440674ac
tiffany <tiffany@stormbit.net>
2013-03-30 15:58:53 -0400

    New string functions

--- src/util/ilstring.h
+++ src/util/ilstring.h
@@ -13,6 +13,7 @@ const il_string *il_string_static(const char *s);
 il_string *il_string_new(const char *s, int len);
 il_string *il_string_copy(const il_string *s);
 char *il_string_cstring(const il_string *s, size_t *len);
+int /*success*/ il_string_resize(il_string *self, size_t size);
 int il_string_verify(const il_string *s);
 il_string *il_string_ref(void* s);
 void il_string_unref(void* s);
@@ -20,6 +21,8 @@ il_string *il_string_sub(il_string *s, int p1, int p2);
 int il_string_cmp(const il_string *a, const il_string *b);
 int il_string_byte(const il_string *s, int pos);
 il_string *il_string_format(const char *fmt, ...);
+int /*success*/ il_string_cat(il_string *self, const il_string *str);
+int /*success*/ il_string_catchars(il_string *self, const char *chars);
 
 #define il_l il_string_static
 #define il_StoC(s) il_string_cstring(s, NULL)
