4c481fcd968baf6191d159f1442cfef406bddc80
tiffany <tiffany@stormbit.net>
2013-03-30 16:46:17 -0400

    Change path's internal structure to use il_string
    It was stupid to not do that initially...

--- src/asset/path.c
+++ src/asset/path.c
@@ -1,13 +1,33 @@
 #include "path.h"
 
+#include <unistd.h>
+
+#include "util/log.h"
+
 ilA_path* ilA_path_string(il_string *path)
 {
-    ilA_path* p = calloc(1, sizeof(ilA_path));
+    ilA_path* p;
+    if (il_string_byte(path, 0) == '~') {
+        char *home = getenv("HOME");
+        if (home) {
+            p = ilA_path_chars(home);
+        } else {
+            il_error("Attempted to parse string starting with ~ and no $HOME is set.");
+            return NULL;
+        }
+    } else if (il_string_byte(path, 0) != '/') {
+        p = ilA_path_cwd();
+        if (!p) {
+            return NULL;
+        }
+    } else {
+        p = calloc(1, sizeof(ilA_path));
+    }
     p->path = il_string_ref(path);
     p->nodes = mowgli_list_create();
-    char *s, *end;
+    char *s, *ptr, *end;
     size_t len, size;
-    for (s = p->nodeptr = il_string_cstring(path, &size); s; s = strchr(s, '/'), end = strchr(s, '/')) {
+    for (s = ptr = il_string_cstring(path, &size); s; s = strchr(s, '/'), end = strchr(s, '/')) {
         if (!end) {
             end = s + size;
         }
@@ -17,9 +37,10 @@ ilA_path* ilA_path_string(il_string *path)
         } else if (strncmp(s, "..", len) == 0) {
             mowgli_node_delete(p->nodes->tail, p->nodes);
         } else {
-            mowgli_node_add(s, mowgli_node_create(), p->nodes);
+            mowgli_node_add(il_string_sub(path, ptr - s, len), mowgli_node_create(), p->nodes);
         }
     }
+    free(ptr);
     return p;
 }
 
@@ -28,12 +49,23 @@ ilA_path* ilA_path_chars(const char *path)
     return ilA_path_string(il_string_new(path, strlen(path)+1));
 }
 
+ilA_path* ilA_path_cwd()
+{
+    char buf[4096];
+    char *cwd = getcwd(buf, 4096);
+    if (!cwd) {
+        il_error("cwd too big");
+        return NULL;
+    }
+    return ilA_path_chars(cwd);
+}
+
 void ilA_path_free(ilA_path* self)
 {
     il_string_unref(self->path);
-    free(self->nodeptr);
     mowgli_node_t *n, *tn;
     MOWGLI_LIST_FOREACH_SAFE(n, tn, self->nodes->head) {
+        il_string_unref(n->data);
         mowgli_node_delete(n, self->nodes);
         mowgli_node_free(n);
     }
@@ -45,14 +77,8 @@ il_string *ilA_path_tostr(const ilA_path* self)
     il_string *str = il_string_new(NULL, 0), *temp;
     mowgli_node_t *n;
     MOWGLI_LIST_FOREACH(n, self->nodes->head) {
-        char *start = n->data,
-             *end = strchr(start, '/');
-        if (!end) {
-            end = self->nodeptr + self->path->length;
-        }
-        temp = il_string_new(start, end-start);
+        temp = n->data;
         il_string_cat(str, temp);
-        il_string_unref(temp);
         if (n->next) {
             il_string_catchars(str, "/");
         }
@@ -60,7 +86,27 @@ il_string *ilA_path_tostr(const ilA_path* self)
     return str;
 }
 
-int ilA_path_cmp(const ilA_path* a, const ilA_path* b);
+int ilA_path_cmp(const ilA_path* a, const ilA_path* b)
+{
+    mowgli_node_t *an, *bn = b->nodes->head;
+    il_string *as, *bs;
+    MOWGLI_LIST_FOREACH(an, a->nodes->head) {
+        if (!bn) {
+            break;
+        }
+        as = an->data;
+        bs = bn->data;
+        if (il_string_cmp(as, bs) != 0) {
+            return 1;
+        }
+        bn = bn->next;
+    }
+    if (a->nodes->count < b->nodes->count) {
+        return -1;
+    }
+    return 0;
+}
+
 ilA_path* ilA_path_concat(const ilA_path* a, const ilA_path* b);
 void ilA_path_relativeTo(ilA_path* self, const ilA_path* path);
 
