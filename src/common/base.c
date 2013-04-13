d0f4917bb2ea0a6c202cdda7b2fe4424810dbbc0
tiffany <tiffany@stormbit.net>
2013-04-13 15:39:54 -0400

    Type metadata setters/getters

--- src/common/base.c
+++ src/common/base.c
@@ -43,6 +43,34 @@ void *il_metadata_set(void *obj, const char *key, void *data)
     return NULL;
 }
 
+void *il_type_metadata_get(void* obj, const char *key)
+{
+    // HASH_FIND_STR (head, key_ptr, item_ptr)
+    il_base_metadata *entry;
+    HASH_FIND_STR(((il_type*)obj)->metadata, key, entry);
+    if (entry) {
+        return entry->value;
+    }
+    return NULL;
+}
+
+void *il_type_metadata_set(void *obj, const char *key, void *data)
+{
+    // HASH_REPLACE_STR (head,keyfield_name, item_ptr, replaced_item_ptr)
+    il_base_metadata *entry;
+    HASH_FIND_STR(((il_type*)obj)->metadata, key, entry);
+    if (entry) {
+        void *old = entry->value;
+        entry->value = data;
+        return old;
+    }
+    entry = calloc(1, sizeof(il_base_metadata));
+    entry->key = key;
+    entry->value = data;
+    HASH_ADD_STR(((il_base*)obj)->metadata, key, entry);
+    return NULL;
+}
+
 size_t il_sizeof(void* obj)
 {
     size_t size = ((il_base*)obj)->size;
