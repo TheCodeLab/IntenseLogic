b8cd737113814b7db0958c41e28c53370f874280
tiffany <tiffany@stormbit.net>
2013-04-17 18:31:20 -0400

    Document matrix.lua
    Inline documentation using LDoc.
    
    I update my documentation to https://lymiahugs.com/~tiffany/il/doc/modules/common.matrix.html

--- script/common/matrix.lua
+++ script/common/matrix.lua
@@ -1,3 +1,6 @@
+---
+-- Common matrix operations for 3D graphics
+-- @author tiffany
 local ffi = require "ffi"
 
 require "scalar_defs"
@@ -34,36 +37,64 @@ local function unm(a)
     return wrap(ffi.C.il_mat_invert(a, nil));
 end
 
-local function wrap(ptr)
+--- Wraps a cdata with a metatable
+-- @param ptr Pointer to cdata
+-- @treturn tab Wrapper table
+local function matrix.wrap(ptr)
     local obj = {}
     obj.ptr = ptr
     setmetatable(obj, {__index=matrix, __mul=mul, __unm=unm});
     return obj;
 end
-matrix.wrap = wrap;
+local wrap = matrix.wrap;
 
+--- Returns whether or not the parameter is a matrix
+-- @treturn bool
 function matrix.check(m)
     return type(m) == "table" and ffi.istype(matrix.type, m.ptr)
 end
 
+--- FFI ctype of matrix
 matrix.type = ffi.typeof "il_mat";
 
+--- Identity matrix
 matrix.identity = wrap(ffi.C.il_mat_identity(nil));
 
+--- Creates a new matrix
+-- @treturn matrix Created matrix
 function matrix.create()
     return wrap(ffi.C.il_mat_new());
 end
 
+--- Produces a translation using the provided vector
+-- The matrix provided is:
+-- 1 0 0 x
+-- 0 1 0 y
+-- 0 0 1 z
+-- 0 0 0 w
+-- @tparam vec4 Translation vector
+-- @treturn matrix Translation matrix
 function matrix.translate(v)
     assert(vector4.check(v))
     return wrap(ffi.C.il_mat_translate(v.ptr, nil));
 end
 
+--- Produces a scaling matrix using the provided vector
+-- The matrix provided is:
+-- x 0 0 0
+-- 0 y 0 0
+-- 0 0 z 0
+-- 0 0 0 w
+-- @tparam vec4 Scaling vector
+-- @treturn matrix Scaling matrix
 function matrix.scale(v)
     assert(vector4.check(v))
     return wrap(ffi.C.il_mat_scale(v.ptr, nil))
 end
 
+--- Creates a rotation matrix
+-- @tparam quaternion a The quaternion to create the matrix with
+-- @treturn matrix A matrix that produces the rotation provided
 function matrix.rotate(a, v)
     if type(a) == "table" then
         assert(quaternion.check(a))
@@ -76,6 +107,12 @@ function matrix.rotate(a, v)
     end
 end
 
+--- Creates a projection matrix
+-- @tparam number fovy Field of view, top to bottom (not left to right, a fovy of 70 is actually on the large end)
+-- @tparam number aspect Aspect ratio of the screen
+-- @tparam number znear Clipping plane for objects close to the camera (set as far away as comfortable, to preserve Z buffer precision)
+-- @tparam number zfar Clipping plane for objects far from the camera (set as close as comfortable, to preserve Z buffer precision)
+-- @treturn matrix Projection matrix
 function matrix.perspective(fovy, aspect, znear, zfar)
     return wrap(ffi.C.il_mat_perspective(nil, fovy, aspect, znear, zfar))
 end
