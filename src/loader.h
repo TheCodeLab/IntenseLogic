0371fb9c110be6ec736eb59ab4e319bced00a2a4
tiffany <tiffany@stormbit.net>
2013-04-21 00:30:29 -0400

    Switched to module loader (broken)
    Broken because none of the modules have bootstrap functions yet, and
    maybe a few bugs remain.
    
    You need to `ln -s build modules` in order to run it now, I should put
    that out there.

--- src/loader.h
+++ src/loader.h
@@ -3,9 +3,9 @@
 
 struct il_config; // TODO: config file structure
 
-int /*success*/ il_loadmod(const char *name, struct il_config *config);
+int /*success*/ il_loadmod(const char *name, int argc, char **argv);
 
-typedef int /*success*/ (*il_bootstrap_fn)(struct il_config *config);
+typedef int /*success*/ (*il_bootstrap_fn)(int argc, char **argv);
 
 #endif
 
