2121d1f029384cf5f50ac78d72c629044002cca0
tiffany <tiffany@stormbit.net>
2013-04-25 02:14:37 -0400

    Forgot to track some files

---
+++ src/graphics/bindable.c
@@ -0,0 +1,41 @@
+#include "bindable.h"
+
+void ilG_bindable_bind(const ilG_bindable *self, void *obj)
+{
+    if (!self->bind) {
+        return;
+    }
+    self->bind(obj);
+}
+
+void ilG_bindable_action(const ilG_bindable *self, void *obj)
+{
+    if (!self->action) {
+        return;
+    }
+    self->action(obj);
+}
+
+void ilG_bindable_unbind(const ilG_bindable *self, void *obj)
+{
+    if (!self->unbind) {
+        return;
+    }
+    self->unbind(obj);
+}
+
+void ilG_bindable_swap(const ilG_bindable **self, void **ptr, void *new)
+{
+    if (*ptr == new) {
+        return;
+    }
+    if (*self && *ptr) {
+        ilG_bindable_unbind(*self, *ptr);
+    }
+    *ptr = new;
+    *self = il_cast(il_typeof(new), "il.graphics.bindable");
+    if (*self && *ptr) {
+        ilG_bindable_bind(*self, *ptr);
+    }
+}
+
