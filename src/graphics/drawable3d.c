0f060be06be73d80e36cf09bdd502c8dff749ffc
tiffany <tiffany@stormbit.net>
2013-04-25 02:14:37 -0400

    Forgot to track some files

---
+++ src/graphics/drawable3d.c
@@ -0,0 +1,22 @@
+#include "drawable3d.h"
+
+#include "graphics/tracker.h"
+
+static void drawable3d_init(void* self)
+{
+    ilG_drawable3d *drawable = self;
+    ilG_drawable3d_assignId(drawable);
+}
+
+il_type ilG_drawable3d_type = {
+    .typeclasses = NULL,
+    .storage = NULL,
+    .constructor = &drawable3d_init,
+    .destructor = NULL,
+    .copy = NULL,
+    .name = "il.graphics.drawable3d",
+    .registry = NULL,
+    .size = sizeof(ilG_drawable3d),
+    .parent = NULL
+};
+
