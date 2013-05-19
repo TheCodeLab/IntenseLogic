6e4b072a2d916d5c4d80e07f47170287ea4301f0
tiffany <tiffany@stormbit.net>
2013-05-19 06:44:08 -0500

    Add assertions to base.c

--- src/common/base.c
+++ src/common/base.c
@@ -1,18 +1,21 @@
 #include "base.h"
 
 #include "util/log.h"
+#include "util/assert.h"
 
 char *strdup(const char*);
 size_t strnlen(const char*,size_t);
 
 void *il_ref(void *obj)
 {
+    il_return_null_on_fail(obj);
     ((il_base*)obj)->refs++;
     return obj;
 }
 
 void il_unref(void* obj)
 {
+    il_return_on_fail(obj);
     il_base *base = obj;
     if (!base) {
         return;
@@ -39,6 +42,7 @@ void il_unref(void* obj)
 
 void il_weakref(void *obj, void **ptr)
 {
+    il_return_on_fail(obj && ptr);
     il_base *base = obj;
     IL_APPEND(base->weak_refs, (il_base**)ptr);
     *ptr = obj;
@@ -46,6 +50,7 @@ void il_weakref(void *obj, void **ptr)
 
 void il_weakunref(void *obj, void **ptr)
 {
+    il_return_on_fail(obj && ptr);
     il_base *base = obj;
     size_t i;
     for (i = 0; i < base->weak_refs.length; i++) {
@@ -60,6 +65,7 @@ void il_weakunref(void *obj, void **ptr)
 
 void *il_storage_get(il_storage **md, const char *key, size_t *size, enum il_storagetype *tag)
 {
+    il_return_null_on_fail(md && key);
     il_storage *entry;
     HASH_FIND_STR(*md, key, entry);
     if (entry) {
@@ -79,6 +85,7 @@ void *il_storage_get(il_storage **md, const char *key, size_t *size, enum il_sto
 
 void il_storage_set(il_storage **md, const char *key, void *data, size_t size, enum il_storagetype tag)
 {
+    il_return_on_fail(md && key && data);
     int is_array = (tag & IL_ARRAY_BIT) == IL_ARRAY_BIT;
     size_t nmemb = is_array? size:1, i;
     void *buf = NULL, *old;
@@ -178,6 +185,7 @@ void il_base_set(il_base* self, const char *key, void *data, size_t size, enum i
 
 ilE_registry *il_base_registry(il_base *self)
 {
+    il_return_null_on_fail(self);
     if (!self->registry) {
         self->registry = ilE_registry_new();
     }
@@ -186,6 +194,7 @@ ilE_registry *il_base_registry(il_base *self)
 
 ilE_registry *il_type_registry(il_type *self)
 {
+    il_return_null_on_fail(self);
     if (!self->registry) {
         self->registry = ilE_registry_new();
     }
@@ -194,6 +203,7 @@ ilE_registry *il_type_registry(il_type *self)
 
 size_t il_sizeof(const il_type* self)
 {
+    il_return_val_on_fail(self, 0);
     size_t size = self->size;
     if (size < sizeof(il_base)) {
         il_warning("Size of type %s is abnormally small: %zu bytes, should be at least %zu bytes", self->name, size, sizeof(il_base));
@@ -207,11 +217,13 @@ size_t il_sizeof(const il_type* self)
 
 il_type *il_typeof(void *obj)
 {
+    il_return_null_on_fail(obj);
     return ((il_base*) obj)->type;
 }
 
 void *il_new(il_type *type)
 {
+    il_return_null_on_fail(type);
     il_base *obj = calloc(1, il_sizeof(type));
     il_init(type, obj);
     return obj;
@@ -219,6 +231,7 @@ void *il_new(il_type *type)
 
 void il_init(il_type *type, void *obj)
 {
+    il_return_on_fail(type && obj);
     il_base *base = obj;
     base->refs = 1;
     base->type = type;
@@ -233,6 +246,7 @@ void il_init(il_type *type, void *obj)
 
 void *il_copy(void *obj)
 {
+    il_return_null_on_fail(obj);
     il_type *cur = il_typeof(obj);
     il_base *new = calloc(1, il_sizeof(cur));
     new->refs = 1;
@@ -248,11 +262,13 @@ void *il_copy(void *obj)
 
 const char *il_name(il_type *type)
 {
+    il_return_null_on_fail(type);
     return type->name;
 }
 
 const void *il_cast(il_type* T, const char *to)
 {
+    il_return_null_on_fail(T && to);
     il_typeclass *tc;
     HASH_FIND_STR(T->typeclasses, to, tc);
     return tc;
@@ -260,6 +276,7 @@ const void *il_cast(il_type* T, const char *to)
 
 void il_impl(il_type* T, void *impl)
 {
+    il_return_on_fail(T && impl);
     il_typeclass *tc = impl;
     HASH_ADD_KEYPTR(hh, T->typeclasses, tc->name, strlen(tc->name), tc);
 }
