a216f2d9a25d335f44f5d13dd9ee7848f26b0624
tiffany <tiffany@stormbit.net>
2013-07-24 11:12:49 -0400

    Document math

--- src/math/matrix.h
+++ src/math/matrix.h
@@ -1,24 +1,37 @@
+/** @file matrix.h
+ * @brief Matrix operations
+ */
+
 #ifndef IL_MATRIX_H
 #define IL_MATRIX_H
 
 #include "vector.h"
 #include "quaternion.h"
 
+/** 4x4 matrix */
 typedef float *il_mat;
 
 il_mat il_mat_new();
 void il_mat_free(il_mat m);
 il_mat il_mat_copy(il_mat m);
 
+/** Each vector is a row */
 il_mat il_mat_set(il_mat m, il_vec4 a, il_vec4 b, il_vec4 c, il_vec4 d);
 il_mat il_mat_mul(const il_mat a, const il_mat b, il_mat m);
 il_vec4 il_mat_mulv(const il_mat a, const il_vec4 b, il_vec4 v);
+/** Creates a translation matrix from a vector */
 il_mat il_mat_translate(const il_vec4 v, il_mat m);
+/** Creates a scaling matrix from a vector */
 il_mat il_mat_scale(const il_vec4 v, il_mat m);
+/** Creates an identity matrix */
 il_mat il_mat_identity(il_mat m);
+/** Creates a perspective transform matrix */
 il_mat il_mat_perspective(il_mat m, float fovy, float aspect, float znear, float zfar);
+/** Creates a rotation matrix */
 il_mat il_mat_rotate(const il_quat q, il_mat m);
+/** Inverts a matrix */
 il_mat il_mat_invert(const il_mat a, il_mat m);
+/** Transposes the elements of a matrix over the diagonal */
 il_mat il_mat_transpose(const il_mat a, il_mat res);
 
 #endif
--- src/math/quaternion.h
+++ src/math/quaternion.h
@@ -1,3 +1,7 @@
+/** @file quaternion.h
+ * @brief Quaternion operations
+ */
+
 #ifndef IL_QUATERNION_H
 #define IL_QUATERNION_H
 
@@ -9,9 +13,13 @@ il_quat il_quat_copy(il_quat q);
 
 il_quat il_quat_set(il_quat q, float x, float y, float z, float w);
 il_quat il_quat_mul(const il_quat a, const il_quat b, il_quat q);
+/** Linear interpolation between two quaternions, using the t coefficient from 0 to 1 */
 il_quat il_quat_lerp(const il_quat a, const il_quat b, float t, il_quat q);
+/** Converts Euler angles to a quaternion */
 il_quat il_quat_fromYPR(float yaw, float pitch, float roll, il_quat q);
+/** Converts to quaternion from Axis-Angle representation */
 il_quat il_quat_fromAxisAngle(float x, float y, float z, float a, il_quat q);
+/** Normalizes a quaternion to keep it as a rotation */
 il_quat il_quat_normalize(const il_quat a, il_quat q);
 float il_quat_len(const il_quat a);
 float il_quat_dot(const il_quat a, const il_quat b);
