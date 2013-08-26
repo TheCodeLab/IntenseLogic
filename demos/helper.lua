657c5044d6a24f411004b958800df4bb2d00d77a
tiffany <tiffany@stormbit.net>
2013-08-25 23:20:33 -0400

    Move demo camera code to helper library

---
+++ demos/helper.lua
@@ -0,0 +1,79 @@
+local file          = require 'asset.file'
+local input         = require 'input.input'
+local quaternion    = require 'math.quaternion'
+local vector3       = require 'math.vector3'
+local frame         = require 'graphics.gui.frame'
+local text          = require 'graphics.gui.text'
+local event         = require 'common.event'
+local camera        = require 'graphics.camera'
+local matrix        = require 'math.matrix'
+
+local helper = {}
+
+function helper.camera(ctx, root)
+    local cam = camera()
+    ctx.camera = cam
+    cam.projection_matrix = matrix.perspective(75, 4/3, 2, 2000).ptr
+    cam.positionable.position = vector3(0, 0, 0).ptr
+    cam.sensitivity = .01
+    cam.movespeed = vector3(1,1,1).ptr
+
+    local first_mouse = true
+    local mousemove = function(reg, name, xabs, yabs, x, y)
+        if first_mouse then first_mouse = false return end
+        if not input.get "mouse left" then return end
+        local yaw = quaternion(vector3(0, 1, 0), x * cam.sensitivity)
+        local pitch = quaternion(vector3(1, 0, 0), y * cam.sensitivity)
+        local rot = quaternion.wrap(cam.positionable.rotation) * yaw * pitch
+        cam.positionable.rotation = rot.ptr
+    end
+
+    local georgia = file.load "georgia.ttf"
+    local camera_pos_label = frame()
+    camera_pos_label.context = ctx
+    camera_pos_label:setPosition(5,-19, 0, 1)
+    root:addChild(camera_pos_label)
+    local render_pos = function(pos)
+        local label = text(ctx, "en", "ltr", "latin", georgia, 14, tostring(pos))
+        camera_pos_label:label(label, {1,1,1,1}, "left middle")
+    end
+
+    local fps_label = frame()
+    fps_label.context = ctx
+    fps_label:setPosition(5, 5, 0, 0)
+    root:addChild(fps_label)
+    local render_fps = function(f)
+        local label = text(ctx, "en", "ltr", "latin", georgia, 14, string.format("FPS: %.1f", tonumber(f)))
+        fps_label:label(label, {1,1,1,1}, "left middle")
+    end
+
+    local tick = function(reg, name)
+        local get = function(k)
+            local b, _ = input.get(k)
+            return b and 1 or 0
+        end
+        local x = get("A") - get("D")
+        local z = get("S") - get("W")
+        local y = get("F") - get("R")
+        local r = get("Q") - get("E")
+        local v = vector3(x,y,z) * vector3.wrap(cam.movespeed)
+        v = v * quaternion.wrap(cam.positionable.rotation)
+        cam.positionable.position = (vector3.wrap(cam.positionable.position) + v).ptr
+        local bank = quaternion(vector3(0, 0, 1), r * cam.sensitivity * 4)
+        cam.positionable.rotation = (quaternion.wrap(cam.positionable.rotation) * bank).ptr
+        render_pos(vector3(cam.positionable.position))
+        render_fps(1/ctx:averageFrametime())
+        cam.projection_matrix = matrix.perspective(75, ctx.width/ctx.height, 2, 2000).ptr
+    end
+
+    local close = function(reg, name)
+        event.event(event.registry, "shutdown")
+    end
+
+    event.register(event.registry, "tick", tick)
+    event.register(event.registry, "input.mousemove", mousemove)
+    event.register(event.registry, "graphics.close", close)
+end
+
+return helper
+
--- demos/teapots/main.lua
+++ demos/teapots/main.lua
@@ -27,6 +27,7 @@ local lightpass     = require "graphics.lightpass"
 local file          = require "asset.file"
 local heightmap     = require "graphics.heightmap"
 local skyboxpass    = require "graphics.skyboxpass"
+local helper        = require "demos.helper"
 
 math.randomseed(os.time())
 
@@ -90,12 +91,6 @@ for i = 0, width*width*width-1 do
     --print(box.position)
 end
 
-c.camera = camera()
-c.camera.projection_matrix = matrix.perspective(75, 4/3, 2, 2000).ptr
-c.camera.positionable.position = vector3(0, 0, 0).ptr
-c.camera.sensitivity = .01
-c.camera.movespeed = vector3(1,1,1).ptr
-
 lights = {
     {vector3(50, 50, 50),   250,    vector3(.4, .4, .4)},
     {vector3(40, 5, 55),    25,     vector3(.3, .4, 1)},
@@ -111,59 +106,5 @@ for _, v in pairs(lights) do
     l:add(c)
 end
 
-local first_mouse = true
-function mousemove(reg, name, xabs, yabs, x, y)
-    if first_mouse then first_mouse = false return end
-    if not input.get "mouse left" then return end
-    local yaw = quaternion(vector3(0, 1, 0), x * c.camera.sensitivity)
-    local pitch = quaternion(vector3(1, 0, 0), y * c.camera.sensitivity)
-    local rot = quaternion.wrap(c.camera.positionable.rotation) * yaw * pitch
-    c.camera.positionable.rotation = rot.ptr
-end
-
-local georgia = file.load "georgia.ttf"
-local camera_pos_label = frame()
-camera_pos_label.context = c
-camera_pos_label:setPosition(5,-19, 0, 1)
-root:addChild(camera_pos_label)
-function render_pos(pos)
-    local label = text(c, "en", "ltr", "latin", georgia, 14, tostring(pos))
-    camera_pos_label:label(label, {1,1,1,1}, "left middle")
-end
-
-local fps_label = frame()
-fps_label.context = c
-fps_label:setPosition(5, 5, 0, 0)
-root:addChild(fps_label)
-function render_fps(f)
-    local label = text(c, "en", "ltr", "latin", georgia, 14, string.format("FPS: %.1f", tonumber(f)))
-    fps_label:label(label, {1,1,1,1}, "left middle")
-end
-
-function tick(reg, name)
-    local get = function(k)
-        local b, _ = input.get(k)
-        return b and 1 or 0
-    end
-    local x = get("A") - get("D")
-    local z = get("S") - get("W")
-    local y = get("F") - get("R")
-    local r = get("Q") - get("E")
-    local v = vector3(x,y,z) * vector3.wrap(c.camera.movespeed)
-    v = v * quaternion.wrap(c.camera.positionable.rotation)
-    c.camera.positionable.position = (vector3.wrap(c.camera.positionable.position) + v).ptr
-    local bank = quaternion(vector3(0, 0, 1), r * c.camera.sensitivity * 4)
-    c.camera.positionable.rotation = (quaternion.wrap(c.camera.positionable.rotation) * bank).ptr
-    render_pos(vector3(c.camera.positionable.position))
-    render_fps(1/c:averageFrametime())
-    c.camera.projection_matrix = matrix.perspective(75, c.width/c.height, 2, 2000).ptr
-end
-
-function close(reg, name)
-    event.event(event.registry, "shutdown")
-end
-
-event.register(event.registry, "tick", tick)
-event.register(event.registry, "input.mousemove", mousemove)
-event.register(event.registry, "graphics.close", close)
+helper.camera(c, root)
 
