23b2a65a8a32b657a785a08a59f20cfd77b3b175
Lymia Aluysia <lymiahugs@gmail.com>
2014-02-02 02:26:31 -0600

    More dist work.

--- dist.sh
+++ dist.sh
@@ -9,26 +9,34 @@ version=$(git describe --dirty --always --tags)
 _32bit() {
   export CFLAGS="-m32"
   export LINKFLAGS="-m32"
+  export mingw_pre="i586"
 }
 _64bit() {
   export CFLAGS="-m64"
   export LINKFLAGS="-m64"
+  export mingw_pre="x86_64-w64"
 }
 linux() {
   export platform="linux"
   export dysuffix=".so"
   export binsuffix=""
-  export CFLAGS="-L/usr/lib/x86_64-linux-gnu "$CFLAGS
+
+  export CC=clang
+  export CXX=clang++
+}
+windows() {
+  export platform="mingw"
+  export dysuffix=".dll"
+  export binsuffix=".exe"
+
+  export CC="$mingw_pre-mingw32msvc-gcc"
+  export CXX="$mingw_pre-mingw32msvc-g++"
 }
 case "$1" in
-  "linux-32")
-    _32bit
-    linux
-    ;;
-  "linux-64")
-    _64bit
-    linux
-    ;;
+  "linux-32") _32bit; linux   ;;
+  "linux-64") _64bit; linux   ;;
+  "win-32"  ) _32bit; windows ;;
+  "win-64"  ) _64bit; windows ;;
   *)
     echo "!!! Unknown platform $1"
     exit 1
