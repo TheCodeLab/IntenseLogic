80c56a324bdabf8dc21943d476083b50620e0891
tiffany <tiffany@stormbit.net>
2014-03-29 01:10:15 -0400

    Add temporary code to make ilutil work right

--- src/util/util.c
+++ src/util/util.c
@@ -2,9 +2,17 @@
 
 void il_logger_init();
 
+const char **il_preload_ilutil() // TODO: Make this a soft dependency
+{
+    static const char *deps[] = {
+        "ilcommon",
+        NULL
+    };
+    return deps;
+}
+
 int il_load_ilutil() 
 {
-    il_load_module("ilcommon", NULL);
     il_logger_init();
     return 0;
 }
