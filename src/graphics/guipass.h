5668ec1b0758c4c86438cd35d6c1fe891d558436
tiffany <tiffany@stormbit.net>
2014-04-13 13:12:24 -0400

    Fix macro

--- src/graphics/guipass.h
+++ src/graphics/guipass.h
@@ -11,7 +11,7 @@ typedef struct ilG_gui ilG_gui;
 
 extern const ilG_renderable ilG_gui_renderer;
 
-#define ilG_gui_wrap(p) ilG_renderable_wrap(p, &ilG_gui_renderer)
+#define ilG_gui_wrap(p) ilG_renderer_wrap(p, &ilG_gui_renderer)
 
 ilG_gui *ilG_gui_new(struct ilG_gui_frame *root);
 
